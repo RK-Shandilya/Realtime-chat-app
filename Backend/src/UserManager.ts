4449d81460583971a5fc0d5eb329cd39fd0c89e9
Rupesh <rkshandilya001@gmail.com>
2024-12-29 23:52:39 +0530

    working on the backend

---
+++ Backend/src/UserManager.ts
@@ -0,0 +1,65 @@
+import { OutgoingMessage } from "./messages/outgoingMessages"
+import WebSocket from 'ws';
+
+interface User {
+    name : string;
+    id : string;
+    conn: WebSocket
+}
+
+interface Room {
+    name : string;
+    users : User[]
+}
+
+export class UserManager {
+    private rooms : Map<string, Room>;
+    constructor() {
+        this.rooms = new Map<string, Room>();
+    }
+
+    addUser(name: string, userId: string, roomId: string, socket: WebSocket) {
+        if(!this.rooms.has(roomId)) {
+            this.rooms.set(roomId, {name, users: []})
+        }
+        this.rooms.get(roomId)?.users.push({
+            id: userId,
+            name,
+            conn: socket
+        })
+        socket.addEventListener('close', (event) => {
+            this.removeUser(roomId, userId);
+        });
+    }
+
+    removeUser(roomId: string, userId: string) {
+        console.log("removed user");
+        const users = this.rooms.get(roomId)?.users;
+        if (users) {
+            users.filter(({id}) => id !== userId);
+        }
+    }
+
+    getUser(roomId: string, userId: string): User | null {
+        const user = this.rooms.get(roomId)?.users.find((({id}) => id === userId));
+        return user ?? null;
+    }
+
+    broadcast(roomId: string, userId: string, message: OutgoingMessage) {
+        const user = this.getUser(roomId, userId);
+        if (!user) {
+            console.error("User not found");
+            return;
+        }
+        const room = this.rooms.get(roomId);
+        if(!room) {
+            return;
+        }
+        room.users.forEach(({conn, id}) => {
+            if(id !== userId) {
+                console.log("outgoing message " + JSON.stringify(message))
+                conn.send(JSON.stringify(message))
+            }
+        });
+    }
+}
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,28 +1,105 @@
-import express from "express"
-import WebSocket from "ws"
-import http from "http"
+import express from 'express';
+import WebSocket from 'ws';
+import http from 'http';
+import { UserManager } from './UserManager';
+import { InMemoryStore } from './store/InMemoryStore';
+import { IncomingMessage, SupportedMessage } from "./messages/IncomingMessage";
+import { OutgoingMessage, SupportedMessage as OutgoingSupportedMessages } from "./messages/outgoingMessages";
 
-const app = express()
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-const server = http.createServer(app)
+const userManager = new UserManager();
+const store = new InMemoryStore();
 
-const wss = new WebSocket.Server({server})
+function originIsAllowed(origin: string): boolean {
+    return true;
+}
 
-const clients: WebSocket[] = [];
+wss.on('connection', (ws: WebSocket, req: http.IncomingMessage) => {
+    const origin = req.headers.origin || '';
 
-wss.on("connection",(ws : WebSocket)=> {
-    console.log('New Client connected')
-    clients.push(ws)
-    ws.on("message",(message: string)=> {
-        const msg = message.toString();
-        clients.forEach((clientsWS)=>{
-            if(clientsWS != ws) {
-                clientsWS.send(msg)
+    if (!originIsAllowed(origin)) {
+        ws.close(1008, 'Origin not allowed');
+        console.log((new Date()) + ' Connection from origin ' + origin + ' rejected.');
+        return;
+    }
+
+    console.log((new Date()) + ' Connection accepted.');
+
+    ws.on('message', (message: WebSocket.Data) => {
+        // Todo add rate limiting logic here
+        console.log('received: %s', message);
+        if (typeof message === 'string') {
+            try {
+                messageHandler(ws, JSON.parse(message));
+            } catch (e) {
+                console.error(e);
             }
-        })
-    })
-})
+        }
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
 
 server.listen(3000, () => {
-    console.log(`server is running at PORT 3000`)
-})
+    console.log(`server is running at PORT 3000`);
+});
+
+function messageHandler(ws: WebSocket, message: IncomingMessage) {
+    if (message.type === SupportedMessage.JoinRoom) {
+        const payload = message.payload;
+        userManager.addUser(payload.name, payload.userId, payload.roomId, ws);
+    }
+
+    if (message.type === SupportedMessage.SendMessage) {
+        const payload = message.payload;
+        const user = userManager.getUser(payload.roomId, payload.userId);
+
+        if (!user) {
+            console.error("User not found in the db");
+            return;
+        }
+        let chat = store.addChat(payload.userId, user.name, payload.roomId, payload.message);
+        if (chat === undefined) {
+            return;
+        }
+
+        const outgoingPayload: OutgoingMessage= {
+            type: OutgoingSupportedMessages.AddChat,
+            payload: {
+                chatId: chat.id,
+                roomId: payload.roomId,
+                message: payload.message,
+                name: user.name,
+                upvotes: 0
+            }
+        }
+        userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+    }
+    
+    if (message.type === SupportedMessage.UpvoteMessage) {
+        const payload = message.payload;
+        const chat = store.upvote(payload.userId, payload.roomId, payload.chatId);
+        console.log("inside upvote")
+        if (!chat) {
+            return;
+        }
+        console.log("inside upvote 2")
+
+        const outgoingPayload: OutgoingMessage= {
+            type: OutgoingSupportedMessages.UpdateChat,
+            payload: {
+                chatId: payload.chatId,
+                roomId: payload.roomId,
+                upvotes: chat.upvotes.length
+            }
+        }
+
+        console.log("inside upvote 3")
+        userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+    }
+}
